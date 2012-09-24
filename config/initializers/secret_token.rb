8411dc96d7987250e51690e86664b915a0738a58
Juan Pablo Escobar <juannpablo@gmail.com>
2012-09-23 20:40:41 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kudyn::Application.config.secret_token = '4c8a939c260a7de3c7d349a310708f62021d3c283ed94b4f5a53cc63b1af1aaa3209ad71c6f41ec5aecf2546892d3f87d43aa748fab174224ccefe984cfbf6d5'

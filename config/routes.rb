96f1016b805cb37af57ec0f9f2f736f83dceeb0c
Juan Pablo Escobar <juannpablo@gmail.com>
2012-10-06 17:27:33 -0500

    Registro de usuarios con devise. Agregado CanCan gem para autorizacion con roles. Agregado modelo de grupos.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Kudyn::Application.routes.draw do
+
+  resources :groups
+
+  devise_for :users
+
   get "home/index"
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
   root :to => 'home#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end

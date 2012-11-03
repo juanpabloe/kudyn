03d1a705b70ed020caf2bce0a732f15546daa011
Fernando <nndo1991@gmail.com>
2012-11-03 15:50:31 -0600

    Arreglo de interfaz en vista new session devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  
+  
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
-
+  
+  def after_sign_in_path_for(user) 
+    home_index_path
+  end
+  
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @users = User.all
+    if not current_user
+    redirect_to new_user_session_path
+    end
   end
 
 end

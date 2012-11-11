32e4bb5a31c883c1275d414c5d7ab27c92243ded
Juan Pablo Escobar <juannpablo@gmail.com>
2012-11-11 13:14:29 -0600

    Se agregaron abilities y flash notice styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
+# encoding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  
+
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to root_url, :alert => "No estás autorizado para acceder a esta sección"
   end
   
   def after_sign_in_path_for(user) 
